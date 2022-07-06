95d2184fec465bc2a283ab801b43cf283228809a
d-evyn <hello@darianrosebrook.com>
2022-07-06 06:59:42 -0700

    Added env for search and basic results

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,31 +1,34 @@
+// Require api key from this file, hidden from git.
 const { NEWS_API_KEY } = require("../../env.js");
+
+// Setting defaults for path and params
 const searchDefaults = {
 	path: "top-headlines",
 	params: {
 		country: "us",
-		apiKey: NEWS_API_KEY,
 	},
 };
 const stringifyParams = (params) => {
+	// force that api key into the params, stringify all params
+	params.apiKey = NEWS_API_KEY;
 	return Object.keys(params)
 		.map((key) => {
 			return `${key}=${params[key]}`;
 		})
 		.join("&");
 };
-const newsAPI = {};
-newsAPI.search = async (searchObj = searchDefaults) => {
+const search = async (searchObj = searchDefaults) => {
 	const urlPath = searchObj.path;
 	const url = `https://newsapi.org/v2/${urlPath}?${stringifyParams(
 		searchObj.params
 	)}`;
 	fetch(url)
 		.then((res) => res.json())
-		.then((data) => {
-			return data;
-		})
+		.then((data) => data)
 		.catch((err) => {
 			console.log(err);
 		});
 };
-exports.newsAPI = newsAPI;
+module.exports = {
+	search,
+};
